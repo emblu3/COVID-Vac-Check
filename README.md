e4c03584cb905f004251bb4c6aa89e10a0e9cbe6
Esteban Mendoza <estmendoza@ucla.edu>
2021-01-22 17:18:09 -0800

    keep running script until page data is read

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # COVID-Vac-Check
-Check to see if Riverside County changed COVID-19 vaccination clinic registration information. A change might indicate that the vaccination date has passed and has been removed, or more dates have been added.
+
+## rh-check.py
+
+Screipt that checks if Riverside County changed COVID-19 vaccination clinic registration information. A change might indicate that the vaccination date has passed and has been removed, or more dates have been added.
+
+## rh-sites.py
+
+Script that checks the Riverside County COVID-19 vaccination registration website and prints/opens all the open registration links.
---
+++ rh-sites.py
@@ -0,0 +1,41 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup 
+import time, smtplib, sys, webbrowser, zlib
+
+def scrapeSite():
+    # Web scraping function
+    my_url = 'https://www.rivcoph.org/COVID-19-Vaccine' #web URL
+    uClient = uReq(my_url) #open URL
+    page_html = zlib.decompress(uClient.read(), 16+zlib.MAX_WBITS)
+    uClient.close() #close URL
+    page_soup = soup(page_html, 'html.parser') #parse URL data
+
+    tables = page_soup.find(id='dnn_ctr2947_HtmlModule_lblContent')
+
+    links = []
+    for link in tables.find_all('a'):
+        links.append(link.get('href'))
+
+    b = webbrowser.get('chrome')
+    for url in links:
+        if 'Full.png' in url:
+            pass
+        else:
+            b.open(url)
+            print(url)
+    print('\n\nDONE!\n')
+
+def forever():
+    while True:
+        try:
+            scrapeSite()
+            break
+        except:
+            pass
+        else:
+            break
+
+forever()
+
+
+
